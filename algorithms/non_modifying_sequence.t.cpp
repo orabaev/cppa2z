2a92cb93e8932e3591bde1909542d424bec7a7fd
orabaev <orabaev@gmail.com>
2016-10-04 18:48:02 -0700

    add std::any_of

--- algorithms/non_modifying_sequence.t.cpp
+++ algorithms/non_modifying_sequence.t.cpp
@@ -2,20 +2,36 @@
 #include <algorithm>
 #include <vector>
 
-TEST_CASE( "all_of", "[std] [algorithm]" ) {
+TEST_CASE( "all_of", "[std] [algorithm] [non modifying]" ) {
     
     SECTION( "all elements in the range match" ) {
-        std::vector<int> vec{2,4,6,8,10};
+        std::vector<int> vec{2,4,6,8,10,12,14};
         auto is_even = [](int i) { return i % 2 == 0; };
-
         REQUIRE( std::all_of(vec.begin(), vec.end(), is_even) );
     } 
 
     SECTION( "not all elements in the range match" ) {
-        std::vector<int> vec{1,1,0,1,1};
+        std::vector<int> vec{1,1,0,1,1,1};
         auto is_one = [](int i) { return i == 1; };
-
         REQUIRE_FALSE( std::all_of(vec.begin(), vec.end(), is_one) );
+    } 
+
+}
+
+TEST_CASE( "any_of", "[std] [algorithm] [non modifying] [not none_of]" ) {
+    
+    auto is_one = [](int i) { return i == 1; };
+    auto is_two = [](int i) { return i == 2; };
+
+    SECTION( "some of the elements in the range match" ) {
+        std::vector<int> vec{1,2,3,2,5,6,7};
+        REQUIRE( std::any_of(vec.begin(), vec.end(), is_one) );
+        REQUIRE( std::any_of(vec.begin(), vec.end(), is_two) );
+    } 
+
+    SECTION( "none of the elements in the range match" ) {
+        std::vector<int> vec{1,1,3,3,5,6,7};
+        REQUIRE_FALSE( std::any_of(vec.begin(), vec.end(), is_two) );
     } 
 
 }
