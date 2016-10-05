ff18e3053c718b39af76e11cc496b7dba85bc717
orabaev <orabaev@gmail.com>
2016-10-04 21:03:18 -0700

    add std::adjucent_find

--- algorithms/non_modifying_sequence.t.cpp
+++ algorithms/non_modifying_sequence.t.cpp
@@ -2,6 +2,29 @@
 #include <algorithm>
 #include <vector>
 
+TEST_CASE( "adjacent_find", "[std] [algorithm] [non modifying]" ) {
+    
+    SECTION( "two consecutive elements found in the range at position 2" ) {
+        std::vector<int> vec{1,2,3,3,5,6,7};
+        auto it = std::adjacent_find(vec.begin(), vec.end());
+        REQUIRE( it != vec.end() );
+
+        auto found_at_pos = std::distance(vec.begin(), it);
+        REQUIRE( found_at_pos == 2 );
+    } 
+
+    SECTION( "using predicates, two consecutive upper case characters found in the range at position 3" ) {
+        std::vector<char> vec{'a','B','c','D','E','f','g'};
+        auto both_upper = [](char x, char y) { return std::isupper(x) && std::isupper(y); };
+        auto it = std::adjacent_find(vec.begin(), vec.end(), both_upper);
+        REQUIRE( it != vec.end() );
+
+        auto found_at_pos = std::distance(vec.begin(), it);
+        REQUIRE( found_at_pos == 3 );
+    }
+
+}
+
 TEST_CASE( "all_of", "[std] [algorithm] [non modifying]" ) {
     
     SECTION( "all elements in the range match" ) {
