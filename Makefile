032531c0b0dd374bfcf65efd647cd49a8bccaf9e
orabaev <orabaev@gmail.com>
2016-10-08 17:46:39 -0700

    use cbegin, cend free functions, use const

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = g++
-CFLAGS = -Wall -std=c++11
+CFLAGS = -Wall -std=c++14
 
 default: cppa2z
 	
--- algorithms/non_modifying_sequence.t.cpp
+++ algorithms/non_modifying_sequence.t.cpp
@@ -1,24 +1,31 @@
 #include <catch.hpp>
 #include <algorithm>
 #include <vector>
+#include <string>
 #include <list>
+
+using namespace std;
 
 TEST_CASE( "adjacent_find", "[std] [algorithm] [non modifying]" ) {
     
     SECTION( "two consecutive elements found in the range at position 2" ) {
-        std::vector<int> vec{1,2,3,3,5,6,7};
-        auto it = std::adjacent_find(vec.begin(), vec.end());
-        REQUIRE( it != vec.end() );
-        auto found_at_pos = std::distance(vec.begin(), it);
+        const vector<int> vec{1, 2, 3, 3, 5, 6, 7};
+
+        const auto it = adjacent_find(cbegin(vec), cend(vec));
+
+        REQUIRE( it != cend(vec) );
+        const auto found_at_pos = distance(cbegin(vec), it);
         REQUIRE( found_at_pos == 2 );
     } 
 
     SECTION( "using predicates; two consecutive upper case characters found in the range at position 3" ) {
-        std::vector<char> vec{'a','B','c','D','E','f','g'};
-        auto both_upper = [](char x, char y) { return std::isupper(x) && std::isupper(y); };
-        auto it = std::adjacent_find(vec.begin(), vec.end(), both_upper);
-        REQUIRE( it != vec.end() );
-        auto found_at_pos = std::distance(vec.begin(), it);
+        const string str{"aBcDEfg"};
+        auto both_upper = [](char x, char y) { return isupper(x) && isupper(y); };
+
+        const auto it = adjacent_find(cbegin(str), cend(str), both_upper);
+
+        REQUIRE( it != cend(str) );
+        const auto found_at_pos = distance(cbegin(str), it);
         REQUIRE( found_at_pos == 3 );
     }
 
@@ -26,34 +33,22 @@ TEST_CASE( "adjacent_find", "[std] [algorithm] [non modifying]" ) {
 
 TEST_CASE( "all_of", "[std] [algorithm] [non modifying]" ) {
     
-    SECTION( "all elements in the range match" ) {
-        std::vector<int> vec{2,4,6,8,10,12,14};
+    SECTION( "using predicates; all elements in the range match" ) {
+        const vector<int> vec{2, 4, 6, 8, 10, 12, 14};
         auto is_even = [](int i) { return i % 2 == 0; };
-        REQUIRE( std::all_of(vec.begin(), vec.end(), is_even) );
-    } 
 
-    SECTION( "not all elements in the range match" ) {
-        std::vector<int> vec{1,1,0,1,1,1};
-        auto is_one = [](int i) { return i == 1; };
-        REQUIRE_FALSE( std::all_of(vec.begin(), vec.end(), is_one) );
+        REQUIRE( all_of(cbegin(vec), cend(vec), is_even) );
     } 
 
 }
 
 TEST_CASE( "any_of", "[std] [algorithm] [non modifying] [not none_of]" ) {
     
-    auto is_one = [](int i) { return i == 1; };
-    auto is_two = [](int i) { return i == 2; };
-
     SECTION( "some of the elements in the range match" ) {
-        std::vector<int> vec{1,2,3,2,5,6,7};
-        REQUIRE( std::any_of(vec.begin(), vec.end(), is_one) );
-        REQUIRE( std::any_of(vec.begin(), vec.end(), is_two) );
-    } 
+        const vector<int> vec{1, 2, 3, 2, 5, 6, 7};
+        auto is_one = [](int i) { return i == 1; };
 
-    SECTION( "none of the elements in the range match" ) {
-        std::vector<int> vec{1,1,3,3,5,6,7};
-        REQUIRE_FALSE( std::any_of(vec.begin(), vec.end(), is_two) );
+        REQUIRE( any_of(cbegin(vec), cbegin(vec), is_one) );
     } 
 
 }
@@ -61,8 +56,9 @@ TEST_CASE( "any_of", "[std] [algorithm] [non modifying] [not none_of]" ) {
 TEST_CASE( "count", "[std] [algorithm] [non modifying]" ) {
     
     SECTION( "returns number of 1s in the range" ) {
-        std::vector<int> vec{1,1,2,5,1,1,6};
-        REQUIRE( 4 == std::count(vec.begin(), vec.end(), 1) );
+        const vector<int> vec{1, 1, 2, 5, 1, 1, 6};
+
+        REQUIRE( 4 == count(cbegin(vec), cend(vec), 1) );
     } 
 
 }
@@ -70,39 +66,44 @@ TEST_CASE( "count", "[std] [algorithm] [non modifying]" ) {
 TEST_CASE( "count_if", "[std] [algorithm] [non modifying]" ) {
     
     SECTION( "using predicates; returns number of even numbers in the range" ) {
-        std::vector<int> vec{1,2,3,4,5,6,7};
+        const vector<int> vec{1, 2, 3, 4, 5, 6, 7};
         auto is_even = [](int i) { return i % 2 == 0; };
-        REQUIRE( 3 == std::count_if(vec.begin(), vec.end(), is_even) );
+
+        REQUIRE( 3 == count_if(cbegin(vec), cend(vec), is_even) );
     }
 
 }
 
 TEST_CASE( "equal", "[std] [algorithm] [non modifying]" ) {
     
-    SECTION( "start from the begining; two ranges are equal, comparing vector and list" ) {
-        std::vector<int> vec{1,2,3,4,5,6,7};
-        std::list<int> list{1,2,3,4,5,6,7};
-        REQUIRE( std::equal(vec.begin(), vec.end(), list.begin()) );
+    SECTION( "start from the begining; two ranges are equal; comparing vector and list" ) {
+        const vector<int> vec{1, 2, 3, 4, 5, 6, 7};
+        const list<int>   lst{1, 2, 3, 4, 5, 6, 7};
+
+        REQUIRE( equal(cbegin(vec), cend(vec), cbegin(lst)) );
     }
 
-    SECTION( "start from the 3rd element, two ranges are equal, comparing vector and list" ) {
-        std::vector<int> vec{-1,0,1,2,3,4,5};
-        std::list<int> list{1,2,3,4,5,6,7};
-        REQUIRE( std::equal(vec.begin() + 2, vec.end(), list.begin()) );
+    SECTION( "start from the 3rd element; two ranges are equal; comparing vector and list" ) {
+        const vector<int> vec{-1, 0, 1, 2, 3, 4, 5};
+        const list<int>   lst{1, 2, 3, 4, 5, 6, 7};
+
+        REQUIRE( equal(cbegin(vec) + 2, cend(vec), cbegin(lst)) );
     }
 
     SECTION( "using reverse iterator; two ranges are equal" ) {
-        std::vector<int> vec1{1,2,3,4,5,6,7};
-        std::vector<int> vec2{7,6,5,4,3,2,1};
-        auto reverse_it = vec2.rbegin(); 
-        REQUIRE( std::equal(vec1.begin(), vec1.end(), reverse_it) );
+        const vector<int> vec1{1, 2, 3, 4, 5, 6, 7};
+        const vector<int> vec2{7, 6, 5, 4, 3, 2, 1};
+        const auto reverse_iterator = crbegin(vec2); 
+
+        REQUIRE( equal(cbegin(vec1), cend(vec1), reverse_iterator) );
     }
 
     SECTION( "using predicates; two ranges are equal" ) {
-        std::string s1{"aBCd1fG"};
-        std::string s2{"AbCD1Fg"};
-        auto case_insensitive_compare = [](char x, char y) { return std::toupper(x) == std::toupper(y); };
-        REQUIRE( std::equal(s1.begin(), s1.end(), s2.begin(), case_insensitive_compare) );
+        const string str1{"aBCd1fG"};
+        const string str2{"AbCD1Fg"};
+        auto case_insensitive_compare = [](char x, char y) { return toupper(x) == toupper(y); };
+
+        REQUIRE( equal(cbegin(str1), cend(str1), cbegin(str2), case_insensitive_compare) );
     }
 
 }
