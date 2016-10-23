335af837e092a596899311eb0bb9a7930f148ef4
orabaev <orabaev@gmail.com>
2016-10-22 20:25:47 -0400

    add includes

--- algorithms/set_operations.t.cpp
+++ algorithms/set_operations.t.cpp
@@ -7,11 +7,35 @@ using namespace std;
 
 TEST_CASE( "includes", "[std] [algorithm] [sorting] [binary search]" ) {
     
-    SECTION( "find element in the sorted range" ) {
-        const vector<int> vec{0, 1, 2, 3, 4, 5, 6};
+    SECTION( "sorted vec1 contains all elements from sorted vec2" ) {
+        const vector<int> vec1{0, 1, 2, 3, 4, 5, 6};
+        const vector<int> vec2{2, 3, 6};
 
-        REQUIRE( binary_search(cbegin(vec), cend(vec), 0) );
-        REQUIRE( binary_search(cbegin(vec), cend(vec), 4) );
-        REQUIRE( binary_search(cbegin(vec), cend(vec), 6) );
+        bool found = includes(
+                          cbegin(vec1)
+                        , cend(vec1)
+                        , cbegin(vec2)
+                        , cend(vec2)
+                     );
+
+        REQUIRE( found );
     } 
+
+    SECTION( "descending sorted vec1 contains all elements from descending sorted vec2" ) {
+        const vector<int> vec1{6, 5, 4, 3, 2, 1, 0};
+        const vector<int> vec2{5, 2, 0};
+
+        bool found = includes(
+                          cbegin(vec1)
+                        , cend(vec1)
+                        , cbegin(vec2)
+                        , cend(vec2)
+                        , greater<int>()
+                     );
+
+        REQUIRE( found );
+    }
+
 }
+
+
