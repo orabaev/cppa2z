dcab72f0d85d83da3033e3413170cfd838aa1a67
orabaev <orabaev@gmail.com>
2016-10-22 20:39:50 -0400

    add inplace_merge

--- algorithms/set_operations.t.cpp
+++ algorithms/set_operations.t.cpp
@@ -1,11 +1,10 @@
 #include <catch.hpp>
 #include <algorithm>
 #include <vector>
-#include <list>
 
 using namespace std;
 
-TEST_CASE( "includes", "[std] [algorithm] [sorting] [binary search]" ) {
+TEST_CASE( "includes", "[std] [algorithm] [sorting] [set operations]" ) {
     
     SECTION( "sorted vec1 contains all elements from sorted vec2" ) {
         const vector<int> vec1{0, 1, 2, 3, 4, 5, 6};
@@ -38,4 +37,37 @@ TEST_CASE( "includes", "[std] [algorithm] [sorting] [binary search]" ) {
 
 }
 
+TEST_CASE( "inplace_merge", "[std] [algorithm] [sorting] [set operations]" ) {
+    
+    SECTION( "merge inplace two sorted ranges within vec" ) {
+              vector<int>      vec{0, 3, 5,   1, 2, 4, 6};
 
+        const vector<int> expected{0, 1, 2, 3, 4, 5, 6};
+        
+        auto middle = begin(vec) + 3;
+        REQUIRE ( 1 == *middle);
+
+        inplace_merge(begin(vec), middle, end(vec));
+
+        REQUIRE( expected == vec );
+    } 
+
+    SECTION( "merge inplace two descending sorted ranges within vec" ) {
+              vector<int>      vec{5, 3, 0,   6, 4, 2, 1};
+
+        const vector<int> expected{6, 5, 4, 3, 2, 1, 0};
+        
+        auto middle = begin(vec) + 3;
+        REQUIRE ( 6 == *middle);
+
+        inplace_merge(
+              begin(vec)
+            , middle
+            , end(vec)
+            , greater<int>()
+        );
+
+        REQUIRE( expected == vec );
+    }
+
+}
