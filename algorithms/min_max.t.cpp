1fcd23eeef8226ee902e9b1c4068163ec404d42b
orabaev <orabaev@gmail.com>
2016-10-25 20:58:23 -0400

    add min, min_element

--- algorithms/min_max.t.cpp
+++ algorithms/min_max.t.cpp
@@ -53,7 +53,6 @@ TEST_CASE( "max", "[std] [algorithm] [min max]" ) {
 
         constexpr int max_value = max(init_list);
 
-        REQUIRE( 17 == max_value );
         static_assert(17 == max_value, "max_value should be 17");
     }
 
@@ -77,6 +76,81 @@ TEST_CASE( "max_element", "[std] [algorithm] [min max]" ) {
         const auto abs_max_it = max_element(cbegin(vec), cend(vec), compare_abs);
 
         REQUIRE( -15 == *abs_max_it );
+    }
+
+}
+
+TEST_CASE( "min", "[std] [algorithm] [min max]" ) {
+    
+    SECTION( "return min value" ) {
+        int x = 0;
+        int y = 1;
+
+        const int min_value = min(x, y);
+
+        REQUIRE( 0 == min_value );
+    } 
+
+    SECTION( "return min absolute value" ) {
+        auto compare_abs = [](int x, int y) { return abs(x) < abs(y); };
+
+        const int abs_min_value = min(-10, 5, compare_abs);
+
+        REQUIRE( 5 == abs_min_value );
+    } 
+
+    SECTION( "return min value as constexpr" ) {
+        constexpr int min_value = min(0, 1);
+
+        static_assert(0 == min_value, "min_value should be 1");
+    }
+
+    SECTION( "return min value in the initializer list" ) {
+        const auto init_list = {1, 9, 3, 17}; 
+
+        const int min_value = min(init_list);
+
+        REQUIRE( 1 == min_value );
+    }
+
+    SECTION( "return min absolute value in the initializer list" ) {
+        const auto init_list = {-100, 1, -9, 3, 17}; 
+
+        auto compare_abs = [](int x, int y) { return abs(x) < abs(y); };
+
+        const int abs_min_value = min(init_list, compare_abs);
+
+        REQUIRE( 1 == abs_min_value );
+    }
+
+    SECTION( "return min value in the initializer list as constexpr" ) {
+        constexpr auto init_list = {1, 9, 3, 17}; 
+
+        constexpr int min_value = min(init_list);
+
+        static_assert(1 == min_value, "min_value should be 17");
+    }
+
+}
+
+TEST_CASE( "min_element", "[std] [algorithm] [min max]" ) {
+    
+    SECTION( "return min element in the range" ) {
+        const vector<int> vec{5, 9, 2, 10};
+
+        const auto min_it = min_element(cbegin(vec), cend(vec));
+
+        REQUIRE( 2 == *min_it );
+    } 
+
+    SECTION( "return absolute min element in the range" ) {
+        const vector<int> vec{1, -15, 9, 2, 10};
+
+        auto compare_abs = [](int x, int y) { return abs(x) < abs(y); };
+
+        const auto abs_min_it = min_element(cbegin(vec), cend(vec), compare_abs);
+
+        REQUIRE( 1 == *abs_min_it );
     }
 
 }
