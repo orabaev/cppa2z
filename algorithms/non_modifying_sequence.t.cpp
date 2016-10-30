eba6a76399b47da95ef6a3c401b0ca4f1350e51b
orabaev <orabaev@gmail.com>
2016-10-29 20:07:34 -0400

    grow lexicographical_compare

--- algorithms/non_modifying_sequence.t.cpp
+++ algorithms/non_modifying_sequence.t.cpp
@@ -309,14 +309,31 @@ TEST_CASE( "lexicographical_compare", "[std] [algorithm] [non modifying]" ) {
 
     SECTION( "compares non equal sized ranges lexicographicaly" ) {
         const string str1{"abcd"};
-        const string str2{"abcde"};
+        const string str2{"ef"};
 
         const bool is_less = lexicographical_compare(
                                  cbegin(str1)
                                , cend(str1)
                                , cbegin(str2)
                                , cend(str2)
-                          );
+                             );
+
+        REQUIRE( is_less );
+    }
+
+    SECTION( "case insensitive compares ranges lexicographicaly ignoring cases" ) {
+        const string str1{"abcd"};
+        const string str2{"ef"};
+
+        auto case_insensitive_compare = [](char x, char y) { return toupper(x) < toupper(y); };
+
+        const bool is_less = lexicographical_compare(
+                                 cbegin(str1)
+                               , cend(str1)
+                               , cbegin(str2)
+                               , cend(str2)
+                               , case_insensitive_compare 
+                             );
 
         REQUIRE( is_less );
     }
@@ -330,7 +347,7 @@ TEST_CASE( "lexicographical_compare", "[std] [algorithm] [non modifying]" ) {
                                , cend(str1)
                                , cbegin(str2)
                                , cend(str2)
-                          );
+                             );
 
         REQUIRE_FALSE( is_less );
     }
