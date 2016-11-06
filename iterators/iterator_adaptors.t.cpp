946873429326104354a7a3814119da9f9982d41d
orabaev <orabaev@gmail.com>
2016-11-06 14:15:20 -0500

    add make_move_iterator

--- iterators/iterator_adaptors.t.cpp
+++ iterators/iterator_adaptors.t.cpp
@@ -8,48 +8,6 @@
 
 using namespace std;
 
-TEST_CASE( "inserter", "[std] [algorithm] [sorting] [binary search]" ) {
-    
-    SECTION( "insert values into the set using insert_iterator" ) {
-        const vector<int>     from{3, 4, 2, 5, 1};
-              set<int>          to;
-        const set<int>    expected{1, 2, 3, 4, 5};
-
-        auto output_it = inserter(to, end(to));
-
-        copy(cbegin(from), cend(from), output_it);
-
-        REQUIRE( expected == to );
-    } 
-
-    SECTION( "insert values into the list using insert_iterator" ) {
-        const vector<int>     from{1, 2, 3, 4, 5};
-              list<int>         to;
-        const list<int>   expected{1, 2, 3, 4, 5};
-
-        auto output_it = inserter(to, end(to));
-
-        copy(cbegin(from), cend(from), output_it);
-
-        REQUIRE( expected == to );
-    }
-
-    SECTION( "insert values into the middle of list using insert_iterator" ) {
-        const vector<int>     from{   2, 3, 4   };
-              list<int>         to{1,          5};
-        const list<int>   expected{1, 2, 3, 4, 5};
-
-        auto insert_after = next(begin(to));
-        auto output_it = inserter(to, insert_after);
-
-        copy(cbegin(from), cend(from), output_it);
-
-        REQUIRE( expected == to );
-    }
-
-}
-
-
 TEST_CASE( "back_inserter", "[std] [algorithm] [sorting] [binary search]" ) {
     
     SECTION( "insert values at the end of the vector" ) {
@@ -108,3 +66,61 @@ TEST_CASE( "front_inserter", "[std] [algorithm] [sorting] [binary search]" ) {
     
 }
 
+TEST_CASE( "inserter", "[std] [algorithm] [sorting] [binary search]" ) {
+    
+    SECTION( "insert values into the set using insert_iterator" ) {
+        const vector<int>     from{3, 4, 2, 5, 1};
+              set<int>          to;
+        const set<int>    expected{1, 2, 3, 4, 5};
+
+        auto output_it = inserter(to, end(to));
+
+        copy(cbegin(from), cend(from), output_it);
+
+        REQUIRE( expected == to );
+    } 
+
+    SECTION( "insert values into the list using insert_iterator" ) {
+        const vector<int>     from{1, 2, 3, 4, 5};
+              list<int>         to;
+        const list<int>   expected{1, 2, 3, 4, 5};
+
+        auto output_it = inserter(to, end(to));
+
+        copy(cbegin(from), cend(from), output_it);
+
+        REQUIRE( expected == to );
+    }
+
+    SECTION( "insert values into the middle of list using insert_iterator" ) {
+        const vector<int>     from{   2, 3, 4   };
+              list<int>         to{1,          5};
+        const list<int>   expected{1, 2, 3, 4, 5};
+
+        auto insert_after = next(begin(to));
+        auto output_it = inserter(to, insert_after);
+
+        copy(cbegin(from), cend(from), output_it);
+
+        REQUIRE( expected == to );
+    }
+
+}
+
+TEST_CASE( "make_move_iterator", "[std] [algorithm] [sorting] [binary search]" ) {
+    
+    SECTION( "use move iterator to move(copy) strings from one vector to another" ) {
+        const vector<string>     from{"one", "two", "three"};
+              vector<string>       to;
+        const vector<string> expected{"one", "two", "three"};
+
+        auto move_begin = make_move_iterator(begin(from));
+        auto move_end = make_move_iterator(end(from));
+        
+        copy(move_begin, move_end, back_inserter(to));
+
+        REQUIRE( expected == to );
+        REQUIRE( from.size() == 3);
+    } 
+
+}
