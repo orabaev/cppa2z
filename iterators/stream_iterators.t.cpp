0e929d7a5675519bd3103a71c48c087029f3a194
orabaev <orabaev@gmail.com>
2016-11-06 19:11:49 -0500

    add stream iterators

--- iterators/stream_iterators.t.cpp
+++ iterators/stream_iterators.t.cpp
@@ -1,15 +1,56 @@
+// istream_iterator
+// istreambuf_iterator
+// ostream_iterator
+// ostreambuf_iterator
+
 #include <catch.hpp>
 #include <algorithm>
 #include <sstream>
 #include <string>
-#include <set>
-#include <list>
 
 using namespace std;
 
-TEST_CASE( "output iterator", "[std] [algorithm] [sorting] [binary search]" ) {
+TEST_CASE( "istream_iterator", "[std] [iterator] [stream]" ) {
     
-    SECTION( "ostream_iterator" ) {
+    SECTION( "read and sum numbers from string stream" ) {
+        istringstream sin{"1 2 3 4 5"};
+        const int expected = 1 + 2 + 3 + 4 + 5;
+
+        const int sum = accumulate(
+                              istream_iterator<int>(sin)
+                            , istream_iterator<int>()
+                            , 0
+                        );  
+
+        REQUIRE( expected == sum );
+    } 
+   
+}
+
+TEST_CASE( "istreambuf_iterator", "[std] [iterator] [stream]" ) {
+    
+    SECTION( "demonstrate parallel increment of two istreambuf iterators" ) {
+        istringstream sin("a b");
+        istreambuf_iterator<char> input_it1(sin);
+        istreambuf_iterator<char> input_it2(sin);
+
+        REQUIRE( *input_it1 == 'a' );
+        REQUIRE( *input_it2 == 'a' );
+
+        ++input_it1;
+        REQUIRE( *input_it1 == ' ' );
+        REQUIRE( *input_it2 == 'a' );
+
+        ++input_it2;
+        REQUIRE( *input_it1 == ' ' );
+        REQUIRE( *input_it2 == 'b' );
+    } 
+   
+}
+
+TEST_CASE( "ostream_iterator", "[std] [iterator] [stream]" ) {
+    
+    SECTION( "write delimited elements to string stream" ) {
         const vector<int> vec{1, 2, 3, 4, 5};
         const string expected{"1->2->3->4->5->"};
 
@@ -25,3 +66,27 @@ TEST_CASE( "output iterator", "[std] [algorithm] [sorting] [binary search]" ) {
    
 }
 
+TEST_CASE( "ostreambuf_iterator", "[std] [iterator] [stream]" ) {
+
+    SECTION( "copy elements to string stream using two ostreambuf iterator" ) {
+        const string str{"Hello C++"};
+        
+        ostringstream sout; 
+
+        auto output_it1 = ostreambuf_iterator<char>(sout);
+        auto output_it2 = ostreambuf_iterator<char>(sout);
+        
+        *output_it1 = 'a';
+        REQUIRE( "a" == sout.str() );
+
+        *output_it2 = 'b';
+        REQUIRE( "ab" == sout.str() );
+
+        *output_it1 = 'c';
+        REQUIRE( "abc" == sout.str() );
+
+        *output_it2 = 'd';
+        REQUIRE( "abcd" == sout.str() );
+    } 
+ 
+}
