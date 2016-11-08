3bfc310c227778bcca33123c5116574330aed24d
orabaev <orabaev@gmail.com>
2016-11-07 21:16:26 -0500

    add distance

--- iterators/iterator_operations.t.cpp
+++ iterators/iterator_operations.t.cpp
@@ -12,7 +12,7 @@ using namespace std;
 TEST_CASE( "advance", "[std] [iterator] [operations]" ) {
     
     SECTION( "advance forward" ) {
-        vector<int> vec{0, 1, 2,  3,  4, 5};
+        const vector<int> vec{0, 1, 2,  3,  4, 5};
 
         auto it = cbegin(vec); 
 
@@ -27,6 +27,37 @@ TEST_CASE( "advance", "[std] [iterator] [operations]" ) {
 
         advance(it, -6);
         REQUIRE( 0 == *it );
+    }
+
+}
+
+TEST_CASE( "distance", "[std] [iterator] [operations]" ) {
+    
+    SECTION( "distance from begining to end" ) {
+        const vector<int> vec{0, 1, 2,  3,  4, 5};
+
+        const auto val = distance(cbegin(vec), cend(vec));
+
+        REQUIRE( vec.size() == val );
+    }
+
+    SECTION( "distance from end to beggining" ) {
+        const vector<int> vec{0, 1, 2,  3,  4, 5};
+
+        const auto val = distance(cend(vec), cbegin(vec));
+
+        REQUIRE( -vec.size() == val );
+    }
+
+    SECTION( "distance between elements" ) {
+        const vector<int> vec{0,   1, 2,  3,  4,   5};
+
+        auto it_first = cbegin(vec) + 1;
+        auto it_last  = cbegin(vec) + 4;
+
+        const auto val = distance(it_first, it_last);
+
+        REQUIRE( 3 == val );
     }
 
 }
