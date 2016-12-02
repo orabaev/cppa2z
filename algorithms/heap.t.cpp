670e9cdafbc58ffccf61e272f08fd70cc4ab63b5
orabaev <orabaev@gmail.com>
2016-12-02 14:52:02 -0500

    refactor heap

--- algorithms/heap.t.cpp
+++ algorithms/heap.t.cpp
@@ -1,12 +1,19 @@
 #include <catch.hpp>
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 TEST_CASE( "is_heap", "[std] [algorithm] [heap]" ) {
     
     // max heap
+
+    SECTION( "elements in the range are max heap" ) {
+        vector<int> vec{5, 4, 3, 2, 1};
+
+        REQUIRE( is_heap(cbegin(vec), cend(vec)) );
+    }
 
     SECTION( "elements in the empty range are max heap" ) {
         const vector<int> vec;
@@ -16,17 +23,17 @@ TEST_CASE( "is_heap", "[std] [algorithm] [heap]" ) {
 
     SECTION( "elements in the range are not max heap" ) {
         const vector<int> vec{1, 5, 3, 2, 4};
+
         REQUIRE_FALSE( is_heap(cbegin(vec), cend(vec)) );
     }
 
-    SECTION( "elements in the range are max heap" ) {
-        vector<int> vec{1, 5, 3, 2, 4};
-
-        make_heap(begin(vec), end(vec));
-        REQUIRE( is_heap(cbegin(vec), cend(vec)) );
-    }
-
     // min heap
+
+    SECTION( "elements in the range are min heap" ) {
+        vector<int> vec{1, 2, 3, 4, 5};
+
+        REQUIRE( is_heap(cbegin(vec), cend(vec), greater<int>()) );
+    }
 
     SECTION( "elements in the empty range are min heap" ) {
         const vector<int> vec;
@@ -36,14 +43,8 @@ TEST_CASE( "is_heap", "[std] [algorithm] [heap]" ) {
 
     SECTION( "elements in the range are not min heap" ) {
         const vector<int> vec{7, 5, 3, 2, 4};
+
         REQUIRE_FALSE( is_heap(cbegin(vec), cend(vec), greater<int>()) );
-    }
-
-    SECTION( "elements in the range are not min heap" ) {
-        vector<int> vec{7, 5, 3, 2, 4};
-
-        make_heap(begin(vec), end(vec), greater<int>());
-        REQUIRE( is_heap(cbegin(vec), cend(vec), greater<int>()) );
     }
 
 }
@@ -56,41 +57,35 @@ TEST_CASE( "is_heap_until", "[std] [algorithm] [heap]" ) {
         const vector<int> vec;
 
         const auto last = is_heap_until(cbegin(vec), cend(vec));
+
         REQUIRE( cend(vec) == last );
     }
 
-    SECTION( "returns last iterator where range is not the max heap" ) {
-        const vector<int> vec {1, 5, 3, 2, 4};
+    SECTION( "returns last iterator where range is not max heap" ) {
+        const vector<int> vec {1,   5,   3, 2, 4};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec));
+
         REQUIRE( cbegin(vec) + 1 == last );
+        REQUIRE( 5 == *last );
 
     }
 
     SECTION( "return last iterator of the complete max heap" ) {
-        vector<int> vec {1, 5, 3, 2, 4};
-
-        make_heap(begin(vec), end(vec));
+        vector<int> vec {5, 4, 3, 2, 1};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec));
         REQUIRE( cend(vec) == last );
-
-        const auto last_pos = distance(cbegin(vec), last);
-        REQUIRE( 5 == last_pos );
     }
 
     SECTION( "returns last iterator of the partial max heap" ) {
-        vector<int> vec {1, 5, 3, 2, 4};
-
-        make_heap(begin(vec), end(vec));
-        vec.push_back(10);
-        vec.push_back(100);
+        vector<int> vec {5, 4, 3,   10,   100};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec));
         REQUIRE( cend(vec) != last );
 
         const auto last_pos = distance(cbegin(vec), last);
-        REQUIRE( 5 == last_pos );
+        REQUIRE( 3 == last_pos );
    }
 
    // min heap
@@ -99,41 +94,37 @@ TEST_CASE( "is_heap_until", "[std] [algorithm] [heap]" ) {
         const vector<int> vec;
 
         const auto last = is_heap_until(cbegin(vec), cend(vec), greater<int>());
+
         REQUIRE( cend(vec) == last );
     }
 
-    SECTION( "returns last iterator where range is not the min heap" ) {
-        const vector<int> vec {7, 5, 3, 2, 1};
+    SECTION( "returns last iterator where range is not min heap" ) {
+        const vector<int> vec {5,   1,   3, 2, 4};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec), greater<int>());
+
         REQUIRE( cbegin(vec) + 1 == last );
+        REQUIRE( 1 == *last );
 
     }
 
     SECTION( "return last iterator of the complete min heap" ) {
-        vector<int> vec {4, 5, 3, 2, 1};
-
-        make_heap(begin(vec), end(vec), greater<int>());
+        vector<int> vec {1, 2, 3, 4, 5};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec), greater<int>());
-        REQUIRE( cend(vec) == last );
 
-        const auto last_pos = distance(cbegin(vec), last);
-        REQUIRE( 5 == last_pos );
+        REQUIRE( cend(vec) == last );
     }
 
     SECTION( "returns last iterator of the partial min heap" ) {
-        vector<int> vec {4, 5, 3, 2, 1};
-
-        make_heap(begin(vec), end(vec), greater<int>());
-        vec.push_back(0);
-        vec.push_back(1);
+        vector<int> vec {3, 4, 5,   2,   1};
 
         const auto last = is_heap_until(cbegin(vec), cend(vec), greater<int>());
+
         REQUIRE( cend(vec) != last );
 
         const auto last_pos = distance(cbegin(vec), last);
-        REQUIRE( 5 == last_pos );
+        REQUIRE( 3 == last_pos );
    }
 
 }
@@ -144,6 +135,7 @@ TEST_CASE( "make_heap", "[std] [algorithm] [heap]" ) {
         vector<int> vec{1, 5, 3, 2, 4};
 
         make_heap(begin(vec), end(vec));
+
         REQUIRE( is_heap(cbegin(vec), cend(vec)) );
     }
 
@@ -151,6 +143,7 @@ TEST_CASE( "make_heap", "[std] [algorithm] [heap]" ) {
         vector<int> vec{7, 5, 3, 2, 4};
 
         make_heap(begin(vec), end(vec), greater<int>());
+
         REQUIRE( is_heap(cbegin(vec), cend(vec), greater<int>()) );
     }
 
@@ -158,21 +151,26 @@ TEST_CASE( "make_heap", "[std] [algorithm] [heap]" ) {
 
 TEST_CASE( "push_heap", "[std] [algorithm] [heap]" ) {
     
-    SECTION( "add elements to the vector and applying push_heap operation to make it a max heap" ) {
+    SECTION( "add elements to the vector while applying push_heap operation to make it a max heap" ) {
         vector<int> vec;
 
         vec.push_back(4);
         push_heap(begin(vec), end(vec));
+
         REQUIRE ( 4 == vec.front() );
         REQUIRE( is_heap(cbegin(vec), cend(vec)) );
+
 
         vec.push_back(1);
         push_heap(begin(vec), end(vec));
+
         REQUIRE ( 4 == vec.front() );
         REQUIRE( is_heap(cbegin(vec), cend(vec)) );
 
+
         vec.push_back(7);
         push_heap(begin(vec), end(vec));
+
         REQUIRE ( 7 == vec.front() );
         REQUIRE( is_heap(cbegin(vec), cend(vec)) );
     }
@@ -183,10 +181,8 @@ TEST_CASE( "pop_heap", "[std] [algorithm] [heap]" ) {
     
     SECTION( "move largest element to the back of the range "
              "maintaining max heap property for the rest of the elements" ) {
-        vector<int> vec{1, 5, 7, 9};
+        vector<int> vec{9, 7, 5, 1};
 
-        make_heap(begin(vec), end(vec));
-        
         pop_heap(begin(vec), end(vec));
         REQUIRE( 9 == vec.back() ); 
 
@@ -207,7 +203,7 @@ TEST_CASE( "pop_heap", "[std] [algorithm] [heap]" ) {
         REQUIRE( sorted == vec );
     }
 
-    SECTION( "move smallest element to the back of the range "
+    SECTION( "move smallest element to the back of the vector "
              "maintaining max heap property for the rest of the elements" ) {
         vector<int> vec{9, 5, 1, 7};
 
