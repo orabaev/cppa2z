6b827dc1567fcb17aae70d74453adeb52e523f00
orabaev <orabaev@gmail.com>
2016-12-03 18:33:49 -0500

    refactor algorithms numeric

--- algorithms/numeric_operations.t.cpp
+++ algorithms/numeric_operations.t.cpp
@@ -8,29 +8,40 @@ using namespace std;
 
 TEST_CASE( "accumulate", "[std] [algorithm] [numeric operations]" ) {
     
-    SECTION( "compute sum of all elements in the range" ) {
-        const vector<int> vec{1, 2, 3, 4, 5};
+    SECTION( "compute sum of all elements in the range plus the initial value" ) {
+        const vector<int> vec{1, 2, 3};
 
-        const int sum = accumulate(cbegin(vec), cend(vec), 0);    
+        const int initial_value = 10; 
+        const int expected = initial_value + 1 + 2 + 3;
+
+        const int sum = accumulate(cbegin(vec), cend(vec), initial_value);    
                   
-        REQUIRE( 15 == sum  ); 
+        REQUIRE( expected == sum  ); 
     } 
 
     SECTION( "compute product of all elements in the range" ) {
-        const vector<int> vec{1, 2, 3, 4, 5};
+        const vector<int> vec{5, 10, 100};
+
+        const int initial_value = 1;
+        const int expected = initial_value * 5 * 10 * 100;
 
         auto multiply = [](int x, int y) { return x * y; };
     
-        const int product = accumulate(cbegin(vec), cend(vec), 1, multiply);    
+        const int product = accumulate(
+                                  cbegin(vec)
+                                , cend(vec)
+                                , initial_value
+                                , multiply
+                            );    
                   
-        REQUIRE( 120 == product  ); 
+        REQUIRE( expected == product  ); 
     } 
 
 }
 
 TEST_CASE( "adjacent_difference", "[std] [algorithm] [numeric operations]" ) {
     
-    SECTION( "compute dfference between next element and it previous element writing the result to a different range" ) {
+    SECTION( "compute dfference between next element and its previous element writing the result to the different range" ) {
         const vector<int>      vec{4, 9, 15, 22, 30};
               vector<int>   result;
         const vector<int> expected{4, 5,  6,  7,  8};
@@ -40,7 +51,7 @@ TEST_CASE( "adjacent_difference", "[std] [algorithm] [numeric operations]" ) {
         REQUIRE( expected == result ); 
     } 
 
-    SECTION( "compute absolute dfference between next element and it previous element writing the result to a different range" ) {
+    SECTION( "compute absolute dfference between next element and its previous element writing the result to the different range" ) {
         const vector<int>      vec{4, -9, 15, -22, -30};
               vector<int>   result;
         const vector<int> expected{4,  5,  6,   7,   8};
@@ -61,14 +72,14 @@ TEST_CASE( "inner_product", "[std] [algorithm] [numeric operations]" ) {
         const vector<int> vec1{1, 2, 3};
         const vector<int> vec2{4, 5, 6};
         
-        constexpr int value = 10; 
-        constexpr int expected = (1 * 4)  +  (2 * 5)  +  (3 * 6) + value;
+        const int initial_value = 10; 
+        const int expected = initial_value + (1 * 4)  +  (2 * 5)  +  (3 * 6);
 
         const int result = inner_product(
                                  cbegin(vec1)
                                , cend(vec1) 
                                , cbegin(vec2) 
-                               , value
+                               , initial_value
                            ); 
                   
         REQUIRE( expected == result ); 
@@ -78,8 +89,8 @@ TEST_CASE( "inner_product", "[std] [algorithm] [numeric operations]" ) {
         const vector<int> vec1{1, 2, 3};
         const vector<int> vec2{4, 5, 6};
         
-        constexpr int value = 10; 
-        constexpr int expected = (1 + 4)  *  (2 + 5)  *  (3 + 6) * value;
+        const int initial_value = 10; 
+        const int expected = initial_value * (1 + 4)  *  (2 + 5)  *  (3 + 6);
 
         auto op1 = [](int x, int y) { return x * y; };
         auto op2 = [](int x, int y) { return x + y; };
@@ -88,7 +99,7 @@ TEST_CASE( "inner_product", "[std] [algorithm] [numeric operations]" ) {
                                  cbegin(vec1)
                                , cend(vec1) 
                                , cbegin(vec2) 
-                               , value
+                               , initial_value
                                , op1
                                , op2
                            ); 
@@ -104,7 +115,7 @@ TEST_CASE( "iota", "[std] [algorithm] [numeric operations]" ) {
               vector<int>      vec(5);
         const vector<int> expected{1, 2, 3, 4, 5};
         
-        constexpr int start_value = 1;
+        const int start_value = 1;
         
         iota(begin(vec), end(vec), start_value);
                   
@@ -119,7 +130,6 @@ TEST_CASE( "partial_sum", "[std] [algorithm] [numeric operations]" ) {
               vector<int>      vec{1, 2, 3,  4,  5};
               vector<int>   result;
         const vector<int> expected{1, 3, 6, 10, 15};
-        
         
         partial_sum(cbegin(vec), cend(vec), back_inserter(result));
                   
