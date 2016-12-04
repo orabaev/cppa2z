d2c714082b79e7b26ce3ac1733ee0268addc8c46
orabaev <orabaev@gmail.com>
2016-12-04 12:27:16 -0500

    refactor algorithms sort

--- algorithms/sorting.t.cpp
+++ algorithms/sorting.t.cpp
@@ -14,7 +14,7 @@ TEST_CASE( "is_sorted", "[std] [algorithm] [sorting]" ) {
     } 
 
     SECTION( "the range is not sorted" ) {
-        const vector<int> vec{0, 1, 2,  1000,  3, 4, 4};
+        const vector<int> vec{0, 1, 2,   1000,   3, 4, 4};
 
         REQUIRE_FALSE( is_sorted(cbegin(vec), cend(vec)) );
     } 
@@ -36,7 +36,7 @@ TEST_CASE( "is_sorted_until", "[std] [algorithm] [sorting]" ) {
         
         auto last_pos = distance(cbegin(vec), last_it);
         REQUIRE( 3 == last_pos );
-        REQUIRE( 2 == vec[last_pos] );
+        REQUIRE( 2 == *last_it );
     } 
 
     SECTION( "the range is fully sorted" ) {
@@ -48,7 +48,7 @@ TEST_CASE( "is_sorted_until", "[std] [algorithm] [sorting]" ) {
     }
 
     SECTION( "the range is not sorted until" ) {
-        const vector<int> vec{5, 1, 2, 1000, 3, 4, 4};
+        const vector<int> vec{5,   1, 2, 3, 4, 4};
 
         const auto last_it = is_sorted_until(cbegin(vec), cend(vec));
         
@@ -56,12 +56,13 @@ TEST_CASE( "is_sorted_until", "[std] [algorithm] [sorting]" ) {
     } 
 
     SECTION( "the range is sorted until in descending order" ) {
-        const vector<int> vec{5, 4, 3, 2, 10, 4, 9};
+        const vector<int> vec{5, 4, 3, 2,   10, 4, 9};
 
         const auto last_it = is_sorted_until(cbegin(vec), cend(vec), greater<int>());
 
         auto last_pos = distance(cbegin(vec), last_it);
         REQUIRE( 4 == last_pos );
+        REQUIRE( 10 == *last_it );
     }
 
 }
@@ -132,8 +133,7 @@ TEST_CASE( "partial_sort", "[std] [algorithm] [sorting]" ) {
 
 TEST_CASE( "partial_sort_copy", "[std] [algorithm] [sorting]" ) {
 
-    SECTION( "copy and partialy sort from the range which "
-             "does not provide random access iterator" ) {
+    SECTION( "copy and partialy sort from the range which does not provide random access iterator" ) {
         const list<int>       from{4, 2, 5, 3, 1, 0, 6};
               vector<int>       to(4);
         const vector<int> expected{0, 1, 2, 3};
@@ -148,8 +148,7 @@ TEST_CASE( "partial_sort_copy", "[std] [algorithm] [sorting]" ) {
         REQUIRE( expected == to );    
     } 
 
-    SECTION( "copy and partialy sort in descending order from "
-             "the range which does not provide random access iterator" ) {
+    SECTION( "copy and partialy sort in descending order from the range which does not provide random access iterator" ) {
         const list<int>       from{4, 2, 5, 3, 1, 0, 6};
               vector<int>       to(4);
         const vector<int> expected{6, 5, 4, 3};
@@ -237,7 +236,6 @@ TEST_CASE( "stable_sort", "[std] [algorithm] [sorting]" ) {
             , {"herb",   51}
             , {"andrei", 46}
         };
-        
 
         auto sort_by_age_desc = [](const programmer& l, const programmer& r) { return l.age > r.age; }; 
 
