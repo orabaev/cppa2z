868e3ed8574333f7b9b20f71c5beeb99ca67822f
orabaev <orabaev@gmail.com>
2021-02-16 20:45:47 -0500

    add optional

--- modern_17/utility_types.t.cpp
+++ modern_17/utility_types.t.cpp
@@ -9,6 +9,61 @@ using namespace std;
 
 static int static_i = 0;
 
+
+TEST_CASE( "optional", "[std] [modern] [optional] [C++17]" ) {
+    SECTION( "with no value" ) {
+        optional<string> opt;
+        REQUIRE( opt == nullopt);
+        REQUIRE_FALSE( opt );
+        REQUIRE_FALSE( opt.has_value());
+        REQUIRE( opt.value_or("default value") == "default value");
+        REQUIRE_THROWS_AS( opt.value(), bad_optional_access);
+    }
+
+    SECTION( "with value" ) {
+        optional<string> opt("Hello");
+        REQUIRE( opt != nullopt);
+        REQUIRE( opt.value() == "Hello");
+        REQUIRE( opt.value_or("default value") == "Hello");
+        REQUIRE( *opt == "Hello");
+        REQUIRE( opt->size() == 5);
+    }
+
+    SECTION( "emplace" ) {
+        optional<string> opt;
+        opt.emplace("Hello");
+        REQUIRE( *opt == "Hello");
+    }
+
+    SECTION( "reset" ) {
+        optional<string> opt("Hello");
+        REQUIRE( opt );
+        opt.reset();
+        REQUIRE_FALSE( opt );
+    }
+
+    SECTION( "move" ) {
+        optional<string> opt1("Allocate very very very very very very long string");
+        optional<string> opt2 = move(*opt1);
+        REQUIRE( opt2 == "Allocate very very very very very very long string" );
+        REQUIRE( opt1 != opt2 );
+    }
+
+    SECTION( "assignment" ) {
+        optional<string> opt1("Allocate very very very very very very long string");
+        optional<string> opt2 = opt1;
+        REQUIRE( opt1 == opt2 );
+
+        opt1 = "Another string";
+        REQUIRE( *opt1 == "Another string" );
+    }
+
+    SECTION( "make_optional" ) {
+        auto opt = make_optional(123);
+        REQUIRE( *opt == 123 );
+    }
+}
+
 TEST_CASE( "variant", "[std] [modern] [variant] [C++17]" ) {
     SECTION( "index" ) {
         variant<int, string, double> var; 
@@ -85,8 +140,8 @@ TEST_CASE( "variant", "[std] [modern] [variant] [C++17]" ) {
 
     SECTION( "visit" ) {
         vector<variant<int, string, double>> vec;
-        vec.emplace_back(11);
         vec.emplace_back("Hello");
+        vec.emplace_back(11);
         vec.emplace_back(123.456);
 
         for (auto& var : vec)
@@ -105,7 +160,6 @@ TEST_CASE( "variant", "[std] [modern] [variant] [C++17]" ) {
             }, var);
         }
     }
-
 
     SECTION( "holds_alternative" ) {
         variant<int, string, double> var; 
